30d3c7f36c611f162d3e7a233c5a3ca6820053ee
fahim-arif <fahim1.618555@gmail.com>
2023-07-06 06:21:43 +0600

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by Fahim on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
